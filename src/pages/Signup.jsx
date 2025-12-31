7edae064d3aa91e3bb617f17326dc1f11ba07f82
nafij islam <sahariannafis70@gmail.com>
2026-01-01 00:26:26 +0600

    Add

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import Container from './../components/common/Container';
 
 const Signup = () => {
+  
+  const handleLogin = (event )=>{
+    console.log("login")
+  }
+
   return (
    <Container>
      <div className="flex mx-auto mt-10 overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800 lg:max-w-4xl">
@@ -52,6 +57,20 @@ const Signup = () => {
           </a>
 
           <span className="w-1/5 border-b dark:border-gray-400 lg:w-1/4"></span>
+        </div>
+        <div className="mt-4">
+          <label
+            className="block mb-2 text-sm font-medium text-gray-600 dark:text-gray-200"
+            htmlFor="LoggingName"
+          >
+            Enter Your Name
+          </label>
+          <input
+            id="LoggingName"
+            className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300"
+            type="text"
+            onChange={handleLogin}
+          />
         </div>
 
         <div className="mt-4">
